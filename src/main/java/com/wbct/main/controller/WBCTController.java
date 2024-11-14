3b56ca80c26889fd6bf0185ef828676c031bc308
jgx6r64 <130815415+jgx6r64@users.noreply.github.com>
2024-11-14 22:26:16 +0530

    AnindyaChatterjee

--- src/main/java/com/wbct/main/controller/WBCTController.java
+++ src/main/java/com/wbct/main/controller/WBCTController.java
@@ -1,32 +1,44 @@
 package com.wbct.main.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wbct.main.Entity.Traveller;
 import com.wbct.main.Services.TravelServices;
 
 //import ch.qos.logback.core.model.Model;
-
 @Controller
-public class WBCTController {
+@RestController
+@RequestMapping("/")
+public class WBCTController 
+{
 	@Autowired
 	private TravelServices travelServices;
+	
+//<-------- To Open The Registration Page -------->	
 	
 	@GetMapping("/ApplicationPage")
 	public String openPage(Model model) {
 		model.addAttribute("traveller", new Traveller());
 		return "Application";
 	}
+//<-------- To Open The Sign In Page -------->
+	
 	@GetMapping("/SigninPage")
 	public String openSigninPage(Model model) {
 		model.addAttribute("traveller", new Traveller());
 		return "signin";
 		}
+	
 	@PostMapping("/RegForm")
 	public String SubmitForm(@ModelAttribute("traveller") Traveller traveller, Model model) {
 		boolean msg = travelServices.RegisterUser(traveller);
@@ -38,6 +50,7 @@ public class WBCTController {
 		}
 		return "Application";
 	}
+	
 	@PostMapping("/SigninForm")
 	public String LoginTraveller(@ModelAttribute("traveller") Traveller traveller, Model model) {
 		Traveller validTraveller = travelServices.LoginTraveller( traveller.getEmail(), traveller.getCity());
@@ -50,6 +63,13 @@ public class WBCTController {
 			model.addAttribute("errorMSG", "Invalid Credentials, Please Try Again Later");
 			return "signin";
 		}
+	}
+	
+// <-------- For Google Sign in ------->
+	
+	@GetMapping
+	public Map<String, Object> currentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+		return oAuth2AuthenticationToken.getPrincipal().getAttributes();
 	}
 }
 
